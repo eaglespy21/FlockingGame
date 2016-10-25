7f63daf115a28b4bb3700e6b362efd7de2cbe038
Saurabh <saurabhhindlekar@hotmail.com>
2016-10-24 20:19:41 -0400

    Collisions working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     public float speedCX = 1, speedCY = 1, speedCZ = 1;
     public float speedX, speedY, speedZ;
-
+    public Vector3 moveDirection = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,12 +12,18 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        speedX = Input.GetAxis("Yaw") * Time.deltaTime * speedCX;
-        speedY = Input.GetAxis("Throttle") * Time.deltaTime * speedCY;
-        print(Input.GetAxis("Throttle"));
-        print("Speed"+speedY);
-        speedY = Input.GetAxis("Pitch") * Time.deltaTime * speedCZ;
-        transform.Rotate(0, speedX, speedZ);
-        transform.Translate(0, 0, speedY);
+        CharacterController controller = GetComponent<CharacterController>();
+        speedX = Input.GetAxis("Horizontal") * Time.deltaTime * speedCX;
+        speedY = Input.GetAxis("Vertical") * Time.deltaTime * speedCY;
+        //print(Input.GetAxis("Throttle"));
+        //print("Speed"+speedY);
+        speedZ = Input.GetAxis("Pitch") * Time.deltaTime * speedCZ;
+        transform.Rotate(speedZ, speedX, 0);
+        //moveDirection = new Vector3(speedX, 0, speedY);
+        //moveDirection = transform.TransformDirection(moveDirection);
+        //moveDirection *= speedCX;
+        //controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(transform.forward * speedY);
+        //transform.Translate(0, 0, speedY);
 	}
 }
