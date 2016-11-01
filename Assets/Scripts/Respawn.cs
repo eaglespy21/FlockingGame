0e6e329401fa236c76aea977c0a2c24d9bf2e1b1
s625559029 <625559029@qq.com>
2016-11-01 23:34:35 +0800

    Playable

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Respawn : MonoBehaviour {
     public GameObject Player;
     public GameObject SpawnPoint;
+	public GameObject PlayerCamera;
 	// Use this for initialization
 	void Start () {
         Player = GameObject.FindGameObjectWithTag("Player");
         SpawnPoint = GameObject.FindGameObjectWithTag("Respawn");
+		PlayerCamera = GameObject.FindGameObjectWithTag ("PlayerCamera");
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,6 @@ public class Respawn : MonoBehaviour {
     {
         print("RespawnFunction called");
         Player.transform.position = SpawnPoint.transform.position;
+		PlayerCamera.transform.position = SpawnPoint.transform.position;
     }
 }
--- Assets/Scripts/TimerAndCollectibleScript.cs
+++ Assets/Scripts/TimerAndCollectibleScript.cs
@@ -1,35 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TimerAndCollectibleScript : MonoBehaviour {
-    public float timeLeft = 30;
+	public float timeLeft = 30;
     public float food = 0;
+	public Image heart1;
+	public Image heart2;
+	public Image heart3;
+	public Text time;
+
+	private int lives = 3;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timeLeft -= Time.fixedDeltaTime;
+		heart1.enabled = true;
+		heart2.enabled = true;
+		heart3.enabled = true;
+		timeLeft -= Time.fixedDeltaTime;
+		time.text = timeLeft.ToString();
+		if (lives == 2) {
+			heart1.enabled = false;
+		} else if (lives == 1) {
+			heart1.enabled = false;
+			heart2.enabled = false;
+		} else if (lives <= 0) {
+			heart1.enabled = false;
+			heart2.enabled = false;
+			heart3.enabled = false;
+		}
 	}
-    void OnCollisionEnter(Collision col)
+   /* void OnCollisionEnter(Collision col)
     {
         print("Collied with:" + col);
-    }
+		if(col.collider.CompareTag("apple"))
+		{
+			print("Apple skin");
+			food++;
+			Destroy(col.gameObject);
+		}
+		if(col.collider.CompareTag("tree")) //replace with tag harm which contains all objects that cause harm 
+		{
+			print("Found palm tree");
+			GetComponent<Respawn>().respawnPlayerFunction();
+		}
+    }*/
     void OnTriggerEnter(Collider col)
     {
         print("Collider trigger entered" + col);
-        if(col.gameObject.name == "apple_skin")
+		if(col.CompareTag("apple"))
         {
             print("Apple skin");
+			lives++;
+			if (lives > 3) {
+				lives = 3;
+			}
             food++;
             Destroy(col.gameObject);
         }
-        if(col.gameObject.name == "palm_tree_1") //replace with tag harm which contains all objects that cause harm 
+		else if(col.CompareTag("tree")||col.CompareTag("house")) //replace with tag harm which contains all objects that cause harm 
         {
             print("Found palm tree");
             GetComponent<Respawn>().respawnPlayerFunction();
+			lives--;
         }
     }
 }
